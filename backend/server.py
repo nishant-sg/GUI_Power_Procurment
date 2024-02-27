51408b41e501574361584ec3a44bf8ceaef7a1fa
unknown <nishantgarg.sg@gmail.com>
2024-02-27 20:14:24 +0530

    added basic api to backend, added cors

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 # will run on http://localhost:5000
 app = Flask(__name__)
+
+# Enable CORS for all routes
+CORS(app)  
 
 @app.route('/test')
 def test():
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/test');
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const res = await response.text();
+      setData(res);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data}
         </p>
         <a
           className="App-link"

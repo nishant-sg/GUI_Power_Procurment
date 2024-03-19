c56e27606aea11c54daea8f275ee94e730807aba
Mahak5457 <2020uee1312@mnit.ac.in>
2024-03-19 22:42:35 +0530

    Adding fortend build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
-
   const [data, setData] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const [value, setValue] = React.useState('Select State');
+
+ const handleChange = (event) => {
+
+   setValue(event.target.value);
+
+ };
+
   const fetchData = async () => {
     try {
-      const response = await fetch('http://localhost:5000/test');
+      const response = await fetch("http://localhost:5000/test");
       if (!response.ok) {
-        throw new Error('Failed to fetch data');
+        throw new Error("Failed to fetch data");
       }
       const res = await response.text();
       setData(res);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {data}
+          <b>Power Procurement Risk Analysis Optimizer</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+      <div className="StateSelectDropDown">
+            <select value={value} onChange={handleChange}> 
+            // use {value} for event handling at backend
+              <option value="Select State">Select State</option>
+              <option value="Rajasthan">Rajasthan</option>
+              <option value="Tamil Nadu">Tamil Nadu</option>
+            </select>
+        </div>
+      </body>
     </div>
   );
 }
